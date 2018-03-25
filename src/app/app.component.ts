a0da3096fa7e4872bed47a08a429500783acdce4
Miguel <migueljose@devaid.cl>
2018-03-25 15:02:35 -0300

    Listing images form json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  entries;
+
+  constructor (private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get('https://demo2697834.mockable.io/movies').subscribe(data => {
+        this.entries = data.entries;
+      console.log(this.entries);
+    });
+  }
+
+
 }
